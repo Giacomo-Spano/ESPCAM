86d05fee44af3ebd538c1a6802b8dcc7b4975b13
GIACOMO SPANO <giacomo.spano@libero.it>
2020-03-15 13:58:42 +0100

    Add project files.

---
+++ ESPCAM/ESPCAM.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESPCAM.ino
+ Created:	3/15/2020 1:57:58 PM
+ Author:	giaco
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
